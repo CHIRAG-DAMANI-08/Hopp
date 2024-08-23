8d742aee15fd12bc264dee6482f0c4a3350f7e99
chirag Damani <chiragdamani2003@gmail.com>
2024-08-23 21:37:54 +0530

    first commit

---
+++ app/(root)/(tabs)/rides.tsx
@@ -0,0 +1,13 @@
+import {SafeAreaView} from "react-native-safe-area-context";
+import {Text} from "react-native";
+
+const History  = () => {
+
+    return (
+        <SafeAreaView>
+            <Text>History</Text>
+        </SafeAreaView>
+    );
+}
+
+export default History;
